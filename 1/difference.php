0663b9e528536d9aa40399b7374578ec34ee3ace
GIL Morgan <gilmorgan.perso@gmail.com>
2024-12-01 12:42:30 +0100

    Premier puzzle

---
+++ 1/difference.php
@@ -0,0 +1,26 @@
+<?php
+
+$stream = fopen(__DIR__ . "/input.txt", "r");
+
+$left = [];
+$right = [];
+$diff = [];
+
+while ($line = fgets($stream)) {
+    $split = explode("   " ,$line);
+    $left[] = $split[0];
+    $right[] = $split[1];
+}
+
+sort($left);
+sort($right);
+
+$diff = array_map(
+    function ($intLeft, $intRight) {
+        return abs($intLeft - $intRight);
+    },
+    $left,
+    $right
+);
+
+var_dump(array_sum($diff));
